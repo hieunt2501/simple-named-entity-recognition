4d23f23292866329e17fb7b57a5c18bebc4ef67d
anhkhoivh <vuhoanganhkhoi11@gmail.com>
2021-12-07 21:01:07 +0700

    text area with checklist

--- sner-fe/src/App.js
+++ sner-fe/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+const entitiesState = [
+  {
+    name: "Person",
+    isChosen: true,
+  },
+  {
+    name: "Organization",
+    isChosen: true,
+  },
+  {
+    name: "Location",
+    isChosen: true,
+  },
+];
 
 function App() {
+  const [text, setText] = useState("");
+  const [entities, setEntities] = useState(entitiesState);
+  function setChecklist(i) {
+    setEntities(entities.map((e,index) => {
+      if (i === index) {
+        e.isChosen = !e.isChosen;
+        return e;
+      }
+      else return e;
+    }));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form class="form" >
+        <div class="row">
+          <div class="col-6">
+            <textarea
+              id="text-area"
+              value={text}
+              onChange={(e)=>setText(e.value)}
+              class="form-control"
+            ></textarea>
+          </div>
+          <div class="col-6">
+            {entities.map((entity, i) => (
+              <div>
+                <input
+                  type="checkbox"
+                  id={entity.name}
+                  name={entity.name}
+                  defaultChecked={entity.isChosen}
+                  onChange={(e) => setChecklist(i)}
+                />
+                <label for={entity.name}>{entity.name}</label>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div class="button-row">
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
